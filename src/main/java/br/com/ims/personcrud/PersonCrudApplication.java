2f5fe048d5738be4010838daafb58e905d291c69
ismaelMS <ims.86@hotmail.com>
2023-11-06 09:03:18 -0300

    Project created

---
+++ src/main/java/br/com/ims/personcrud/PersonCrudApplication.java
@@ -0,0 +1,13 @@
+package br.com.ims.personcrud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PersonCrudApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PersonCrudApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/ims/personcrud/PersonCrudApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.ims.personcrud;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PersonCrudApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
